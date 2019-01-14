85fa73b9227a9b4fc7c3961aa7d387fb47048f05
Park JiHye <youo78@naver.com>
2019-01-14 10:08:32 +0900

    2178번 풀었음

--- src/test_2178.java
+++ src/test_2178.java
@@ -3,33 +3,86 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class test_2178 {
-    public static void main(String[] args) throws IOException {
+    static LinkedBlockingQueue<Integer[]> queue;
+    static int[][] memo;
+    static int[][] map;
+    static int n;
+    static int m;
+    public static void main(String[] args) throws IOException, InterruptedException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in),8);
 
         StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int m = Integer.parseInt(stringTokenizer.nextToken());
+        n = Integer.parseInt(stringTokenizer.nextToken());
+        m = Integer.parseInt(stringTokenizer.nextToken());
 
-        int[][] map = new int[n][m];
-        int[][] memo = new int[n][m];
+        map = new int[n][m];
+        memo = new int[n][m];
+        queue = new LinkedBlockingQueue<>(1000000);
         for(int i = 0; i < n; i++){
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            String s = str.nextToken();
             for(int j = 0; j < m; j++){
-                StringTokenizer str = new StringTokenizer(br.readLine());
-                map[i][j] = Integer.parseInt(str.nextToken());
+                map[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
             }
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                int min = 9999;
-                if(i == 0 && j == 0)
-                    memo[i][j] = 1;
-                if(i-1 >= 0){
-                    min = Math.min(min, memo[])
-                }
-            }
+        map[0][0] = 0;
+        System.out.print(BFS(0,0,1));
+
+    }
+
+    public static int BFS(int i, int j,  int p) throws InterruptedException {
+        int sum = 0;
+
+        Integer[] b;
+        if(i == n-1 && j == m-1) {
+            return p;
         }
+
+        if(i-1 >= 0  && map[i-1][j] == 1) {
+           map[i-1][j] = 0;
+           b = new Integer[3];
+           b[0] = i-1;
+           b[1] = j;
+           b[2] = p+1;
+            queue.offer(b);
+        }
+
+        if(i+1 < n && map[i+1][j] == 1){
+            map[i+1][j] = 0;
+            b = new Integer[3];
+            b[0] = i+1;
+            b[1] = j;
+            b[2] = p+1;
+            queue.offer(b);
+        }
+
+        if(j-1 >= 0 && map[i][j-1] == 1) {
+            map[i][j-1] = 0;
+            b = new Integer[3];
+            b[0] = i;
+            b[1] = j-1;
+            b[2] = p+1;
+            queue.offer(b);
+        }
+
+        if(j+1 < m && map[i][j+1] == 1){
+            map[i][j+1] = 0;
+            b = new Integer[3];
+            b[0] = i;
+            b[1] = j+1;
+            b[2] = p+1;
+            queue.offer(b);
+        }
+
+        if(!queue.isEmpty()){
+            Integer[] bucket = queue.take();
+            sum += BFS(bucket[0],bucket[1],bucket[2]);
+        }
+
+        return sum;
     }
 }
